335cf44f4cb531b77b894833ee7b17914a977b5c
github username <github email address>
2018-10-14 22:01:49 -0400

    Controller && validations

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongsController < ApplicationController
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,35 @@
 class Song < ActiveRecord::Base
+   validates :title, presence: true
+   validates :title, uniqueness: {
+     scope: %i[release_year artist_name],
+     message: 'cannot be repeated by artist in the same year'
+   }
+   validates :released, inclusion: { in: [true, false] }
+   with_options if: :released? do |song|
+     song.validates :release_year, presence: true
+     song.validates :release_year, numericality: {
+       less_than_or_equal_to: Date.today.year
+     }
+   end
+
+   def released?
+     released
+   end
+
 end
+
+#
+# - `title`, a `string`
+#   - Must not be blank
+#   - Cannot be repeated by the same artist in the same year
+# - `released`, a `boolean` describing whether the song was ever officially
+#   released.
+#   - Must be `true` or `false`
+# - `release_year`, an `integer`
+#   - Optional if `released` is `false`
+#   - Must not be blank if `released` is `true`
+#   - Must be less than or equal to the current year
+# - `artist_name`, a `string`
+
+#   - Must not be blank
+# - `genre`, a `string`
